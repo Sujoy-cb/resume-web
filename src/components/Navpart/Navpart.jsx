f51728041c79d13b3dba1145d373251e5c5d4451
Sujoy chakraborty <sujoyc070@gmail.com>
2023-10-12 23:37:39 +0600

    li

--- src/components/Navpart/Navpart.jsx
+++ src/components/Navpart/Navpart.jsx
@@ -1,19 +1,19 @@
 const Navpart = () => {
   return (
     <>
-      <div className="navpart h-[100px] bg-bcolor">
+      <div className="navpart bg-bcolor p-5">
         <div className="container mx-auto">
         <div className="nav_items flex justify-between">
           <div className="logo">logo</div>
           <div className="nav_item"> 
             <ul className="flex gap-8">
-                <li className="font-Nunito text-fcolor text-sm font-medium">Dashboard</li>
-                <li className="font-Nunito text-fcolor text-sm font-medium">Upgrade</li>
-                <li className="font-Nunito text-fcolor text-sm font-medium">English</li>
-                <li className="font-Nunito text-fcolor text-sm font-medium">User name</li>
-                <li className="font-Nunito text-fcolor text-sm font-medium">User name</li>
-                <li className="font-Nunito text-fcolor text-sm font-medium">User name</li>
-                <li className="font-Nunito text-fcolor text-sm font-medium">User name</li>
+                <li className="font-Nunito text-fcolor text-base font-medium">Dashboard</li>
+                <li className="font-Nunito text-fcolor text-base font-medium">Upgrade</li>
+                <li className="font-Nunito text-fcolor text-base font-medium">English</li>
+                <li className="font-Nunito text-fcolor text-base font-medium">User name</li>
+                <li className="font-Nunito text-fcolor text-base font-medium">User name</li>
+                <li className="font-Nunito text-fcolor text-base font-medium">User name</li>
+                <li className="font-Nunito text-fcolor text-base font-medium">User name</li>
             </ul>
           </div>
         </div>
