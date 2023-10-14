7a0782a7b6e93763469f37874b2a611a98947476
Sujoy chakraborty <sujoyc070@gmail.com>
2023-10-14 23:40:21 +0600

    logo

--- src/components/Navpart/Navpart.jsx
+++ src/components/Navpart/Navpart.jsx
@@ -1,11 +1,14 @@
+import logo from '../../assets/logo.png'
 const Navpart = () => {
   return (
     <>
       <div className="navpart bg-bcolor p-5">
         <div className="container mx-auto">
         <div className="nav_items flex justify-between">
-          <div className="logo">logo</div>
-          <div className="nav_item"> 
+          <div className="logo">
+            <img src={logo} alt="logo" width={200} height={80} />
+          </div>
+          <div className="nav_item flex items-center"> 
             <ul className="flex gap-8">
                 <li className="font-Nunito text-fcolor text-base font-medium">Dashboard</li>
                 <li className="font-Nunito text-fcolor text-base font-medium">Upgrade</li>
