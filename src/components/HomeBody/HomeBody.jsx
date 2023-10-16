1796ae2631faca41fd622c77ec637c5095095329
Sujoy chakraborty <sujoyc070@gmail.com>
2023-10-16 23:25:05 +0600

    some changes

--- src/components/HomeBody/HomeBody.jsx
+++ src/components/HomeBody/HomeBody.jsx
@@ -1,15 +1,15 @@
 const HomeBody = () => {
   return (
     <>
-      <div className="container flex gap-4 mt-4 mx-auto">
+      <div className="container-full flex gap-4 mx-auto p-2">
         <div className="bg-[#fff] h-40 w-[20%] p-4 shadow-lg shadow-slate-100 rounded-md">
           <h4 className="font-Nunito text-xl text-textColor font-semibold">Fill section</h4>
         </div>
         <div className="bg-[#fff] h-40 w-[50%] p-4 shadow-lg shadow-slate-100 rounded-md">
-          <h4>BUILDING A PERFECT RESUME HAS NEVER BEEN THIS EASY!</h4>
+          <h4 className="font-Nunito text-xl text-textColor font-semibold">BUILDING A PERFECT RESUME HAS NEVER BEEN THIS EASY!</h4>
         </div>
         <div className="bg-[#fff] h-40 w-[30%] p-4 shadow-lg shadow-slate-100 rounded-md">
-          <h4>Preview</h4>
+          <h4 className="font-Nunito text-xl text-textColor font-semibold">Preview</h4>
         </div>
       </div>
     </>
