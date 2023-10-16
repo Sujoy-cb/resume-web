91e4b9589ab262ba7aa15ca88283dd31bde1edb9
Sujoy chakraborty <sujoyc070@gmail.com>
2023-10-16 23:16:15 +0600

    some changes

--- src/components/HomeBody/HomeBody.jsx
+++ src/components/HomeBody/HomeBody.jsx
@@ -1,15 +1,19 @@
-
-
 const HomeBody = () => {
   return (
     <>
-        <div className="flex gap-4 mt-4">
-            <div className="bg-[#fff] h-40 w-[20%] p-4 shadow-lg shadow-slate-100 rounded-md">Fill section</div>
-            <div className="bg-[#fff] h-40 w-[50%] p-4 shadow-lg shadow-slate-100 rounded-md">BUILDING A PERFECT RESUME HAS NEVER BEEN THIS EASY!</div>
-            <div className="bg-[#fff] h-40 w-[30%] p-4 shadow-lg shadow-slate-100 rounded-md">preview</div>
+      <div className="flex gap-4 mt-4">
+        <div className="bg-[#fff] h-40 w-[20%] p-4 shadow-lg shadow-slate-100 rounded-md">
+          <h4>Fill section</h4>
         </div>
+        <div className="bg-[#fff] h-40 w-[50%] p-4 shadow-lg shadow-slate-100 rounded-md">
+          <h4>BUILDING A PERFECT RESUME HAS NEVER BEEN THIS EASY!</h4>
+        </div>
+        <div className="bg-[#fff] h-40 w-[30%] p-4 shadow-lg shadow-slate-100 rounded-md">
+          <h4>Preview</h4>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default HomeBody
+export default HomeBody;
