0346a41f61f5bdad42b8e78b12d63b01b253fd87
Sujoy chakraborty <sujoyc070@gmail.com>
2023-10-17 23:48:46 +0600

    fill section

--- src/components/HomeBody/HomeBody.jsx
+++ src/components/HomeBody/HomeBody.jsx
@@ -2,14 +2,17 @@ const HomeBody = () => {
   return (
     <>
       <div className="container-full flex gap-4 mx-auto p-2">
-        <div className="bg-[#fff] h-40 w-[20%] p-4 shadow-lg shadow-slate-100 rounded-md">
+        <div className="bg-[#fff] w-[20%] p-4 shadow-lg shadow-slate-100 rounded-md">
           <h4 className="font-Nunito text-xl text-textColor font-semibold">Fill section</h4>
+          <p className="font-Nunito text-base text-textColor">Write your details.</p>
         </div>
-        <div className="bg-[#fff] h-40 w-[50%] p-4 shadow-lg shadow-slate-100 rounded-md">
-          <h4 className="font-Nunito text-xl text-textColor font-semibold">BUILDING A PERFECT RESUME HAS NEVER BEEN THIS EASY!</h4>
+        <div className="bg-[#fff] w-[50%] p-4 shadow-lg shadow-slate-100 rounded-md">
+          <h4 className="font-Nunito text-xl text-textColor font-semibold">Establish fact that a reader</h4>
+          <p className="font-Nunito text-base text-textColor">Fill sections with your exact details.</p>
         </div>
-        <div className="bg-[#fff] h-40 w-[30%] p-4 shadow-lg shadow-slate-100 rounded-md">
+        <div className="bg-[#fff] w-[30%] p-4 shadow-lg shadow-slate-100 rounded-md">
           <h4 className="font-Nunito text-xl text-textColor font-semibold">Preview</h4>
+          <p className="font-Nunito text-base text-textColor">Fill sections with your exact details.</p>
         </div>
       </div>
     </>
