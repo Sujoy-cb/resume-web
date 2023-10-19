e13f9652de3dc3bf389c3b0189d5f66f283f03cb
Sujoy chakraborty <sujoyc070@gmail.com>
2023-10-19 23:42:12 +0600

    preview

--- src/components/HomeBody/HomeBody.jsx
+++ src/components/HomeBody/HomeBody.jsx
@@ -1,21 +1,14 @@
 import DataInput from "../DataInput/DataInput";
+import Preview from "../Preview/Preview";
 import Sidebar from "../Sidebar/Sidebar";
 
 const HomeBody = () => {
   return (
     <>
       <div className="container-full flex gap-4 mx-auto p-2">
-      <Sidebar/>
-        
-        <DataInput/>
-        <div className="bg-[#fff] w-[30%] p-4 shadow-lg shadow-slate-100 rounded-md">
-          <h4 className="font-Nunito text-xl text-textColor font-semibold">
-            Preview
-          </h4>
-          <p className="font-Nunito text-base text-textColor">
-            Fill sections with your exact details.
-          </p>
-        </div>
+        <Sidebar />
+        <DataInput />
+        <Preview />
       </div>
     </>
   );
---
+++ src/components/Preview/Preview.jsx
@@ -0,0 +1,16 @@
+const Preview = () => {
+  return (
+    <>
+      <div className="bg-[#fff] w-[30%] p-4 shadow-lg shadow-slate-100 rounded-md">
+        <h4 className="font-Nunito text-xl text-textColor font-semibold">
+          Preview
+        </h4>
+        <p className="font-Nunito text-base text-textColor">
+          Fill sections with your exact details.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Preview;
