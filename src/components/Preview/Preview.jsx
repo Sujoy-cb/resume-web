fb4e7f04bd6ffa9cfb9aa425f16b3602fb7ffaa7
Sujoy chakraborty <sujoyc070@gmail.com>
2023-10-30 23:08:42 +0600

    preview 92%

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -8,13 +8,20 @@ const Preview = () => {
         <p className="font-Nunito text-base text-textColor">
           Fill sections with your exact details.
         </p>
-        <div className="preview_part bg-[#f5f5f5] h-[430px] mt-4"></div>
+        <div className="preview flex justify-center">
+          <div className="preview_part bg-[#f5f5f5] w-[270px] h-[330px] mt-4"></div>
+        </div>
         <div className="grid grid-cols-4 gap-2 mt-4">
           <div className="p1 bg-[#f33737] h-[100px]"></div>
           <div className="p1 bg-[#f33737] h-[100px]"></div>
           <div className="p1 bg-[#f33737] h-[100px]"></div>
           <div className="p1 bg-[#f33737] h-[100px]"></div>
         </div>
+        <button
+              className="flex justify-center items-center bg-bcolor w-[185px] h-[44px] mt-[48px] border border-solid border-[#502acd] rounded font-Nunito text-lg text-[#fff] font-semibold"
+              type="button">
+              Download PDF
+            </button>
       </div>
     </>
   );
