b0593d87092e8229856820cc46cefc1383ac72f7
Sujoy chakraborty <sujoyc070@gmail.com>
2023-11-02 00:52:51 +0600

    overlay

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -10,10 +10,13 @@ const Preview = () => {
         <p className="font-Nunito text-base text-textColor">
           Fill sections with your exact details.
         </p>
-        <div className="preview">
-          <div className="preview_overlay bg-[#000] w-[270px] h-[330px] mx-auto mt-4 z-10">
-            <div className="preview_part bg-[#f5f5f5] w-[270px] h-[330px] mx-auto"></div>
+        <div className="preview relative">
+          <div className="preview_overlay w-[270px] h-[330px] absolute top-0 left-[12px] flex justify-center items-center">
+            <div className="preview_icon w-12 h-12 text-bcolor bg-fcolor border border-fcolor rounded-full flex justify-center items-center cursor-pointer hover:bg-bcolor hover:border-bcolor hover:text-fcolor">
+            <AiOutlineEye className="text-xl hover" />
+            </div>
           </div>
+          <div className="preview_part bg-[#f5f5f5] w-[270px] h-[330px] mx-auto mt-4 p-2">fgdfdfgdfgdfgdfgfg</div>
         </div>
         <div className="grid grid-cols-4 gap-2 mt-4">
           <div className="p1 bg-[#f33737] h-[100px]"></div>
@@ -22,12 +25,6 @@ const Preview = () => {
           <div className="p1 bg-[#f33737] h-[100px]"></div>
         </div>
         <div className="buttons flex justify-center">
-          {/* <button
-            className="flex justify-center items-center bg-[#f5f5f5] w-[170px] h-[44px] mt-[48px] border border-solid border-[#f5f5f5] rounded font-Nunito text-lg text-textColor font-semibold"
-            type="button">
-            <AiOutlineEye className="mr-[5px]" />
-            Preview resume
-          </button> */}
           <button
             className="flex justify-center items-center bg-bcolor w-[170px] h-[44px] mt-[48px] border border-solid border-[#502acd] rounded font-Nunito text-lg text-[#fff] font-semibold"
             type="button">
