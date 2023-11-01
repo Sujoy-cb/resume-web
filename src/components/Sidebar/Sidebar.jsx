0193514c039ff5fb458f7bf4185bc6a0e7b4351d
Sujoy chakraborty <sujoyc070@gmail.com>
2023-11-01 23:52:28 +0600

    sidebar customize

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -18,29 +18,37 @@ const Sidebar = () => {
             Choose the option and write your details.
           </p>
           <div className="choose_part grid grid-rows-4 grid-flow-col gap-3 mt-4">
-            <span className="p-8 bg-[#f5f5f5] rounded-md cursor-pointer">
+            <span className="p-6 bg-[#f5f5f5] rounded-md cursor-pointer">
               <FaUserTie className="text-[40px] text-bcolor mx-auto"/>
+              <p className="font-Nunito text-base text-textColor font-bold text-center">User info</p>
             </span>
             <span className="p-8 bg-[#f5f5f5] rounded-md cursor-pointer">
               <GiNotebook className="text-[40px] text-bcolor mx-auto"/>
+              <p className="font-Nunito text-base text-textColor font-bold text-center">Summary</p>
             </span>
             <span className="p-8 bg-[#f5f5f5] rounded-md cursor-pointer">
               <MdWork className="text-[40px] text-bcolor mx-auto"/>
+              <p className="font-Nunito text-base text-textColor font-bold text-center">Experience</p>
             </span>
             <span className="p-8 bg-[#f5f5f5] rounded-md cursor-pointer">
               <SiBookstack className="text-[40px] text-bcolor mx-auto"/>
+              <p className="font-Nunito text-base text-textColor font-bold text-center">Educations</p>
             </span>
             <span className="p-8 bg-[#f5f5f5] rounded-md cursor-pointer">
               <FaGlobeAmericas className="text-[40px] text-bcolor mx-auto"/>
+              <p className="font-Nunito text-base text-textColor font-bold text-center">Languages</p>
             </span>
             <span className="p-8 bg-[#f5f5f5] rounded-md cursor-pointer">
               <SlBadge className="text-[40px] text-bcolor mx-auto"/>
+              <p className="font-Nunito text-base text-textColor font-bold text-center">Achievements</p>
             </span>
             <span className="p-8 bg-[#f5f5f5] rounded-md cursor-pointer">
               <FaUserGraduate className="text-[40px] text-bcolor mx-auto"/>
+              <p className="font-Nunito text-base text-textColor font-bold text-center">Courses</p>
             </span>
             <span className="p-8 bg-[#f5f5f5] rounded-md cursor-pointer">
               <AiOutlineLaptop className="text-[40px] text-bcolor mx-auto"/>
+              <p className="font-Nunito text-base text-textColor font-bold text-center">Skills</p>
             </span>
           </div>
         </div>
