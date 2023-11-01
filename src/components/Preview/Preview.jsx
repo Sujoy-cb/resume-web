82a10d150555d4a1c5e2748aecae8805d889aaf8
Sujoy chakraborty <sujoyc070@gmail.com>
2023-11-02 00:00:43 +0600

    preview customize

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -3,15 +3,17 @@ import { AiOutlineEye } from "react-icons/ai";
 const Preview = () => {
   return (
     <>
-      <div className="bg-[#fff] w-[30%] p-4 shadow-lg shadow-slate-100 rounded-md">
+      <div className="bg-[#fff] w-[25%] p-4 shadow-lg shadow-slate-100 rounded-md">
         <h4 className="font-Nunito text-xl text-textColor font-semibold">
           Preview
         </h4>
         <p className="font-Nunito text-base text-textColor">
           Fill sections with your exact details.
         </p>
-        <div className="preview flex justify-center">
-          <div className="preview_part bg-[#f5f5f5] w-[270px] h-[330px] mt-4"></div>
+        <div className="preview">
+          <div className="preview_overlay bg-[#000] w-[270px] h-[330px] mx-auto mt-4 z-10">
+            <div className="preview_part bg-[#f5f5f5] w-[270px] h-[330px] mx-auto"></div>
+          </div>
         </div>
         <div className="grid grid-cols-4 gap-2 mt-4">
           <div className="p1 bg-[#f33737] h-[100px]"></div>
@@ -19,13 +21,13 @@ const Preview = () => {
           <div className="p1 bg-[#f33737] h-[100px]"></div>
           <div className="p1 bg-[#f33737] h-[100px]"></div>
         </div>
-        <div className="buttons flex justify-between">
-          <button
+        <div className="buttons flex justify-center">
+          {/* <button
             className="flex justify-center items-center bg-[#f5f5f5] w-[170px] h-[44px] mt-[48px] border border-solid border-[#f5f5f5] rounded font-Nunito text-lg text-textColor font-semibold"
             type="button">
             <AiOutlineEye className="mr-[5px]" />
             Preview resume
-          </button>
+          </button> */}
           <button
             className="flex justify-center items-center bg-bcolor w-[170px] h-[44px] mt-[48px] border border-solid border-[#502acd] rounded font-Nunito text-lg text-[#fff] font-semibold"
             type="button">
