fd9e2a26b4d7acbf2e4e2d4dc84086b6a86e0760
Sujoy chakraborty <sujoyc070@gmail.com>
2023-11-03 22:58:45 +0600

    text area

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,3 +1,9 @@
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+
+import FroalaEditorComponent from 'react-froala-wysiwyg';
 const Summary = () => {
   return (
     <>
@@ -10,7 +16,27 @@ const Summary = () => {
           professional resume in less than 5 minutes.
         </p>
         <div className="summary_input h-[400px]">
-          <textarea className="border border-solid border-[#f5f5f5] bg-[#f5f5f5] w-full h-full p-2 rounded mt-2 outline-none" name="#" id="summaryInput" cols="30" rows="10"></textarea>
+          {/* <textarea
+            className="border border-solid border-[#f5f5f5] bg-[#f5f5f5] w-full h-full p-2 rounded mt-2 outline-none resize-none"
+            name="#"
+            id="summaryInput"
+            cols="30"
+            rows="10"></textarea> */}
+            <FroalaEditorComponent tag='textarea' className="border border-solid border-[#f5f5f5] bg-[#f5f5f5] w-full h-full p-2 rounded mt-2 outline-none resize-none"/>
+        </div>
+        <div className="buttons flex justify-end">
+          <button
+            className="flex justify-center items-center bg-[#f5f5f5] w-[185px] h-[44px] mt-[48px] border border-solid border-[#f5f5f5] rounded font-Nunito text-lg text-textColor font-semibold hover:bg-bcolor hover:text-[#fff] hover:border-[#502acd] transition duration-500 ease-in-out"
+            type="submit">
+            <AiOutlineArrowLeft />
+            Prev
+          </button>
+          <button
+            className="flex justify-center items-center bg-bcolor w-[185px] h-[44px] mt-[48px] border border-solid border-[#502acd] rounded font-Nunito text-lg text-[#fff] font-semibold mr-4"
+            type="submit">
+            Next
+            <AiOutlineArrowRight />
+          </button>
         </div>
       </div>
     </>
