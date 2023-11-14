7e7383255d17088f5f97fcc34ea6365f301502a5
Sujoy chakraborty <sujoyc070@gmail.com>
2023-11-14 23:18:42 +0600

    achieve done

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -1,6 +1,5 @@
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { AiOutlineArrowRight } from "react-icons/ai";
-import { AiOutlinePlusCircle } from "react-icons/ai";
 import FroalaEditorComponent from "react-froala-wysiwyg";
 import "froala-editor/css/froala_style.min.css";
 import "froala-editor/css/froala_editor.pkgd.min.css";
@@ -18,88 +17,14 @@ const Achievements = () => {
         </p>
         <div className="ex_inputs mt-4">
           <h4 className="font-Nunito text-xl text-textColor font-bold">
-            Work Experiences
+            Your achievements
           </h4>
-          <div className="ex_inputItems grid grid-cols-2 gap-4">
-            <div className="company_name mt-4">
-              <label htmlFor="">
-                <input
-                  className="border border-solid border-[#f5f5f5] bg-[#f5f5f5] w-full p-2 rounded mt-2 outline-none"
-                  type="text"
-                  placeholder="Company name"
-                />
-              </label>
-            </div>
-            <div className="job_title mt-4">
-              <label htmlFor="">
-                <input
-                  className="border border-solid border-[#f5f5f5] bg-[#f5f5f5] w-full p-2 rounded mt-2 outline-none"
-                  type="text"
-                  placeholder="Job title"
-                />
-              </label>
-            </div>
-            <div className="grid grid-cols-2 gap-4">
-              <div className="date_of_birth mt-4">
-                <label htmlFor="">
-                  <p className="font-Nunito text-base text-[#777] font-normal">
-                    Start date
-                  </p>
-                  <input
-                    className="border border-solid border-[#f5f5f5] bg-[#f5f5f5] w-full p-2 rounded mt-2 outline-none"
-                    type="date"
-                  />
-                </label>
-              </div>
-              <div className="date_of_birth mt-4">
-                <label htmlFor="">
-                  <p className="font-Nunito text-base text-[#777] font-normal">
-                    End date
-                  </p>
-                  <input
-                    className="border border-solid border-[#f5f5f5] bg-[#f5f5f5] w-full p-2 rounded mt-2 outline-none"
-                    type="date"
-                  />
-                </label>
-              </div>
-              <div className="still_working flex gap-1">
-                <input
-                  id="checkbox"
-                  type="checkbox"
-                />
-                <label
-                  htmlFor="checkbox"
-                  className="font-Nunito text-base text-[#777] font-normal">
-                  Still working
-                </label>
-              </div>
-            </div>
-            <div className="company_address mt-4">
-              <label htmlFor="">
-                <p className="font-Nunito text-base text-[#777] font-normal">
-                  Company address
-                </p>
-                <input
-                  className="border border-solid border-[#f5f5f5] bg-[#f5f5f5] w-full p-2 rounded mt-2 outline-none"
-                  type="text"
-                />
-              </label>
-            </div>
-          </div>
         </div>
         <div className="experience mt-4">
           <p className="font-Nunito text-base text-[#777] font-normal mb-2">
-            Your responsibilities
+            Write something about your achievements
           </p>
           <FroalaEditorComponent tag="textarea" />
-        </div>
-        <div className="add_more flex mt-4 cursor-default">
-          <AiOutlinePlusCircle className="text-xl text-bcolor mt-[2px]" />
-          <span>
-            <p className="font-Nunito text-base text-[#777] font-normal ml-1">
-              Add more options
-            </p>
-          </span>
         </div>
         <div className="buttons flex justify-end">
           <button
