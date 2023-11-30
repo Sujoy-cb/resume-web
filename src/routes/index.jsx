b9f6d327aa794891ec79c0ddff07230ee182f3c1
Sujoy chakraborty <sujoyc070@gmail.com>
2023-11-30 22:45:04 +0600

    up routing done

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import signup from './signup';
 import login from './login';
+import upgrade from "./upgrade";
 import home from "./home";
 
-const router = createBrowserRouter([signup, login, home])
+const router = createBrowserRouter([signup, login, home, upgrade])
 
 
 export default router;
